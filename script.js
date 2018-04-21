cf4884a5ef48dd2b05163e636cd7384602d0d5e9
Student <student@Admins-MacBook-Air.local>
2018-04-21 13:05:34 -0700

    behind

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-/* remove this comment and place your JavaScript here */ 
+/* remove this comment and place your JavaScript here */
+
+let personality;
+
+let head;
+
+let likes;
+
+let pageTitle = document.getElementById('pageTitle');
+
+let pageTitleText = pageTitle.innerHTML;
+
+let tryAgain = document.getElementById('try-again');
+
+let quizWrapper = document.getElementById('quiz-wrapper');
+
+let result = documenet.getElementById('result');
+
+let formSubmit = documenet.getElementById('form-submit');
